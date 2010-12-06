32bf82f828c9ef904dff6d05c098dc2657612e14
legoff.laurent <legoff.laurent@localhost>
2010-12-06 11:45:38 +0000

    - add draw (android, gopher) - resolve some point on matrix transformation

---
+++ draw2d/src/pkg/draw2d/path_adder.go
@@ -0,0 +1,31 @@
+package draw2d
+
+
+import(
+	"freetype-go.googlecode.com/hg/freetype/raster"	
+)
+
+
+type PathAdder struct {
+	adder raster.Adder
+}
+
+
+func floatToPoint(x, y float) raster.Point {
+	return raster.Point{raster.Fix32(x * 256), raster.Fix32(y * 256)}
+}
+
+func tracePath(approximationScale float, adder raster.Adder, paths ...*Path) {
+	pathAdder := &PathAdder{adder}
+	for _, path := range paths {
+		path.TraceLine(pathAdder, approximationScale)
+	}
+}
+
+func (pathAdder *PathAdder) MoveTo(x, y float) {
+	pathAdder.adder.Start(floatToPoint(x, y))
+}
+
+func (pathAdder *PathAdder) LineTo(x, y float) {
+	pathAdder.adder.Add1(floatToPoint(x, y))
+}
