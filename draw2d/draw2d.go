2aaea9baff49518e736f10a3e52e2c23a9f774f8
Laurent Le Goff <legoff.laurent@gmail.com>
2011-09-29 09:55:40 +0200

    add doc

---
+++ draw2d/draw2d.go
@@ -0,0 +1,5 @@
+// Copyright 2011 The draw2d Authors. All rights reserved.
+// created: 29/09/2011 by Laurent Le Goff
+
+// The package draw2d provide a Graphic Context that can draw vectorial figure on surface.
+package draw2d
