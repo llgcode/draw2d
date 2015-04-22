f8fb5a20526d25b8849edf82c49d0f7fce42c629
Laurent Le Goff <legoff.laurent@gmail.com>
2015-04-22 16:22:24 +0200

    Update doc

--- draw2d.go
+++ draw2d.go
@@ -1,5 +1,5 @@
 // Copyright 2010 The draw2d Authors. All rights reserved.
 // created: 13/12/2010 by Laurent Le Goff
 
-// Package draw2d provide a Graphic Context that can draw vector form on canvas.
+// Package draw2d provides a Graphic Context that can draw vector form on canvas.
 package draw2d
