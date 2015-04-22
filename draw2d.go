17aa1056060111ce3ac71d0e9fc3b4d5e808f221
Laurent Le Goff <legoff.laurent@gmail.com>
2015-04-22 10:49:37 +0200

    Change root documentation and rename doc.go in draw2d.go

--- draw2d.go
+++ draw2d.go
@@ -1,5 +1,5 @@
 // Copyright 2010 The draw2d Authors. All rights reserved.
 // created: 13/12/2010 by Laurent Le Goff
 
-// The package draw2d provide a Graphic Context that can draw vectorial figure on surface.
+// Package draw2d provide a Graphic Context that can draw vector form on canvas.
 package draw2d
