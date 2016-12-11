55aea0e8f2cc1def8076f99068f86390048cff5b
gerald1248 <gerald1248@gmail.com>
2016-12-11 02:12:36 +0100

    WIP disabled default font cache

--- font.go
+++ font.go
@@ -144,11 +144,14 @@ func (cache *defaultFontCache) Store(fontData FontData, font *truetype.Font) {
 }
 
 var (
+  /*
 	defaultFonts = &defaultFontCache{
 		fonts:  make(map[string]*truetype.Font),
 		folder: "../resource/font",
 		namer:  FontFileName,
 	}
+  */
 
-	fontCache FontCache = defaultFonts
+	//fontCache FontCache = defaultFonts
+	fontCache FontCache = nil
 )
